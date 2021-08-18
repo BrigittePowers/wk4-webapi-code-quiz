5d63738e72fd6be6963b76de11020c136b2727ea
Brigitte Powers PC <reyhn701@gmail.com>
2021-08-18 16:50:11 -0500

    Added framework for array of questions and answers to script.js

---
+++ script.js
@@ -0,0 +1,67 @@
+// questions and answers
+var question = [
+    {
+        number: 1,
+        question: "This is question 1?",
+        correct: "This is the correct answer",
+        choices: [
+            "This is an incorrect answer",
+            "This is also incorrect but moreso",
+            "This is the correct answer"
+        ]
+    },
+    {
+        number: 2,
+        question: "This is question 2?",
+        correct: "This is the correct answer",
+        choices: [
+            "This is an incorrect answer",
+            "This is also incorrect but moreso",
+            "This is the correct answer"
+        ]
+    },
+    {
+        number: 3,
+        question: "This is question 3?",
+        correct: "This is the correct answer",
+        choices: [
+            "This is an incorrect answer",
+            "This is also incorrect but moreso",
+            "This is the correct answer"
+        ]
+    },
+]
+
+
+// User loads the page
+
+// User sees a button
+
+// On click, user is presented with a question
+
+// and a timer
+
+// When question is answered, another question props
+
+// When question is answered incorrectly, time is taken off timer
+
+// When all ? answered or timer is 0 GAME ENDS
+
+// User can save initials and score
+
+// Score is stored for later viewing
+
+
+/*
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+*/
